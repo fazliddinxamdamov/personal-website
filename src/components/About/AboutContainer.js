9b0f35564620f689cabc24fbd53223253f9b3b26
fazliddin <fazliddinxamdamov102@gmail.com>
2022-02-09 16:49:02 +0500

    hamburger update

--- src/components/About/AboutContainer.js
+++ src/components/About/AboutContainer.js
@@ -18,6 +18,7 @@ function AboutContainer() {
                 <div className={"description"}>
                     <p>Mening ismim Fazliddin Xamdamov.</p>
                     <p>2002-yilda Andijonda tug'ilganman. Hozirda Toshkentda Java Backend yo'nalishi bo'yicha o'z tajribamni oshirib bormoqdaman.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque dolorum mollitia nesciunt quia quibusdam quisquam ullam voluptatum. A atque cupiditate ex modi mollitia odio perferendis rem reprehenderit sint soluta tempora ut vero, voluptatem voluptatibus voluptatum. Aliquam consequatur dicta dolorem enim iure maxime nulla officiis quasi quidem totam! Ad alias aspernatur at commodi corporis ea, enim eos fugit iste, magnam, nemo nostrum odio optio quaerat quam quia quidem quisquam quo suscipit tempora tempore vel veritatis voluptates. Eos itaque iusto repellat, vero voluptate voluptatibus! Adipisci alias aliquam assumenda, atque corporis exercitationem expedita iste itaque labore, odit quaerat ratione saepe sit, ut vitae!</p>
                 </div>
             </div>
         </div>
