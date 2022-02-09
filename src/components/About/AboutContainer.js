6dd294e5e23721abf54a72c09f42e41d6423cb43
fazliddin <fazliddinxamdamov102@gmail.com>
2022-02-09 17:10:20 +0500

    about update

--- src/components/About/AboutContainer.js
+++ src/components/About/AboutContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import './about.css'
 import img from '../home/java.jpg'
 import me from './me.jpg'
+import pdp from './pdp.JPG'
 
 function AboutContainer() {
     return (
@@ -18,9 +19,21 @@ function AboutContainer() {
                 <div className={"description"}>
                     <p>Mening ismim Fazliddin Xamdamov.</p>
                     <p>2002-yilda Andijonda tug'ilganman. Hozirda Toshkentda Java Backend yo'nalishi bo'yicha o'z tajribamni oshirib bormoqdaman.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque dolorum mollitia nesciunt quia quibusdam quisquam ullam voluptatum. A atque cupiditate ex modi mollitia odio perferendis rem reprehenderit sint soluta tempora ut vero, voluptatem voluptatibus voluptatum. Aliquam consequatur dicta dolorem enim iure maxime nulla officiis quasi quidem totam! Ad alias aspernatur at commodi corporis ea, enim eos fugit iste, magnam, nemo nostrum odio optio quaerat quam quia quidem quisquam quo suscipit tempora tempore vel veritatis voluptates. Eos itaque iusto repellat, vero voluptate voluptatibus! Adipisci alias aliquam assumenda, atque corporis exercitationem expedita iste itaque labore, odit quaerat ratione saepe sit, ut vitae!</p>
                 </div>
             </div>
+
+            <div className={"about-description"}>
+                <p></p>
+                <div className={"div-img"}>
+                    <img src={pdp} className={"img"} alt={"not found"}/>
+                </div>
+                <div className={"description"}>
+                    <p>PDP IT Academy.</p>
+                    <p>2002-yilda Andijonda tug'ilganman. Hozirda Toshkentda Java Backend yo'nalishi bo'yicha o'z tajribamni oshirib bormoqdaman.</p>
+                </div>
+            </div>
+
+
         </div>
     )
 }
