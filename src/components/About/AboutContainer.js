59b9ee5942efe89822f6f3efc2e2ad0b7382a927
fazliddin <fazliddinxamdamov102@gmail.com>
2022-02-11 12:12:52 +0500

    Reveal updated

--- src/components/About/AboutContainer.js
+++ src/components/About/AboutContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './about.css'
 import img from '../home/java.jpg'
 import me from './me.jpg'
@@ -6,56 +6,66 @@ import pdp from './pdp.JPG'
 import andijon from './andijon.jpg'
 import Slide from 'react-reveal/Slide';
 import Shake from 'react-reveal/Shake';
+import Reveal from 'react-reveal/Reveal';
+
 
 function AboutContainer() {
+
+    const [post, setPost] = useState(null)
+    useEffect(() => {
+        fetch("http://localhost:8080/about").then(response => response.ok).then(response => {
+            const {body} = response
+        })
+    }, [])
+
     return (
         <div className={"containerAbout"}>
-
-            <div className={"title"}>
-                <Shake><h1>Assalomu alaykum🖐️</h1></Shake>
-                <p>O'zim haqimda qisqacha...</p>
-            </div>
-
             <Slide left>
-                <div className={"about-description"}>
+                <div className={"title"}>
+                    <h1>Assalomu alaykum🖐️</h1>
+                    <p>O'zim haqimda qisqacha...</p>
+                </div>
+            </Slide>
+            <div className={"about-description"}>
+                <Slide top>
                     <div className={"div-img2"}>
                         <img src={me} className={"img"} alt={"not found"}/>
                     </div>
-                    <div className={"description"}>
-                        <p>Mening ismim Fazliddin Xamdamov.</p>
-                        <p>2002-yilda Andijonda tug'ilganman. Hozirda Toshkentda Java Backend yo'nalishi bo'yicha o'z
-                            tajribamni oshirib bormoqdaman.</p>
-                    </div>
-                </div>
-            </Slide>
+                </Slide>
 
-            <Slide left>
-                <div className={"about-description"}>
-                    <p></p>
-                    <div className={"div-img"}>
-                        <img src={andijon} className={"img"} alt={"not found"}/>
-                    </div>
-                    <div className={"description"}>
-                        <p>Andijon(Anjan).</p>
-                        <p>2002-yilda Andijonda tug'ilganman. Hozirda Toshkentda Java Backend yo'nalishi bo'yicha o'z
-                            tajribamni oshirib bormoqdaman.</p>
-                    </div>
+                <div className={"description"}>
+                    <p>Mening ismim Fazliddin Xamdamov.</p>
+                    <p>2002-yilda Andijonda tug'ilganman. Hozirda Toshkentda Java Backend yo'nalishi bo'yicha o'z
+                        tajribamni oshirib bormoqdaman.</p>
                 </div>
-            </Slide>
+            </div>
 
-            <Slide left>
-                <div className={"about-description"}>
-                    <p></p>
-                    <div className={"div-img"}>
-                        <img src={pdp} className={"img"} alt={"not found"}/>
-                    </div>
-                    <div className={"description"}>
-                        <p>PDP IT Academy.</p>
-                        <p>2002-yilda Andijonda tug'ilganman. Hozirda Toshkentda Java Backend yo'nalishi bo'yicha o'z
-                            tajribamni oshirib bormoqdaman.</p>
-                    </div>
+
+            <div className={"about-description"}>
+                <p></p>
+                <div className={"div-img"}>
+                    <img src={andijon} className={"img"} alt={"not found"}/>
                 </div>
-            </Slide>
+                <div className={"description"}>
+                    <p>Andijon(Anjan).</p>
+                    <p>2002-yilda Andijonda tug'ilganman. Hozirda Toshkentda Java Backend yo'nalishi bo'yicha o'z
+                        tajribamni oshirib bormoqdaman.</p>
+                </div>
+            </div>
+
+
+            <div className={"about-description"}>
+                <p></p>
+                <div className={"div-img"}>
+                    <img src={pdp} className={"img"} alt={"not found"}/>
+                </div>
+                <div className={"description"}>
+                    <p>PDP IT Academy.</p>
+                    <p>2002-yilda Andijonda tug'ilganman. Hozirda Toshkentda Java Backend yo'nalishi bo'yicha o'z
+                        tajribamni oshirib bormoqdaman.</p>
+                </div>
+            </div>
+
 
         </div>
     )
