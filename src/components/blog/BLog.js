a4c71621e8da610ad1773fd841cce87bea86f4e0
Fazliddin <fazliddinxamdamov102@gmail.com>
2022-09-15 14:17:00 +0500

    jump to blogItem

--- src/components/blog/BLog.js
+++ src/components/blog/BLog.js
@@ -17,10 +17,38 @@ function Blog() {
                 <div className={'part1'}>
                     <div className={"blog-links"}>
 
-                        <div className={'blog'}><Link to="/"> aaa</Link></div>
-                        <div className={'blog'}><Link to="/"> aaa</Link></div>
-                        {/*<div className={'blog'}><Link to="/"> aaa</Link></div>*/}
-                        {/*<div className={'blog'}><Link to="/"> aaa</Link></div>*/}
+                        <Link to="/blog/api-ozi-nima">
+                            <div className={'blog'}>
+                                <div>
+                                    <img src="https://miro.medium.com/max/875/1*_6Zxe5-EGDkbLlrqIeTsIA.png" alt="Photo not found"/>
+                                </div>
+                                <div>
+                                    <p>15 Sep</p>
+                                    <br/>
+                                    <h2>API o'zi nima?</h2>
+                                    <br/>
+                                    <p>(Application Programming Interface) API — bu Application Programming Interface qisqartmasi bo’lib, u ikkita ilovaga bir-biri bilan gaplashish imkonini beruvchi dasturiy vositachi hisoblanadi. Har safar...</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <br/>
+
+                        <Link to="/blog/agile-metodologiyasi">
+                            <div className={'blog'}>
+                                <div>
+                                    <img src="https://miro.medium.com/max/588/1*_2b6wH3mJ8RkW9BMvY-ydw.png" alt="Photo not found"/>
+                                </div>
+                                <div>
+                                    <p>10 Sep</p>
+                                    <br/>
+                                    <h2>Agile metodologiyasi</h2>
+                                    <br/>
+                                    <p>Ushbu maqolada biz Agile metodologiyasi haqida qisqacha tanishib chiqamiz. Agile metodologiyasi — bu loyihani bir necha bosqichlarga bo’lish orqali boshqarish usuli. Bunga manfaatdor tomonlar bilan doim...</p>
+                                </div>
+                            </div>
+                        </Link>
+
 
                     </div>
 
